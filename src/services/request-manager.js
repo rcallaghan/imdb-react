e241218e56459c3645537f7d5c4da1a22d1f478f
Ryan Callaghan <me@ryancallaghan.com.au>
2016-09-06 00:22:15 +1000

    Removing debounce for now, will reimplement later on.

--- src/services/request-manager.js
+++ src/services/request-manager.js
@@ -5,22 +5,6 @@ function makeRequest (url) {
 	})
 }
 
-// http://davidwalsh.name/javascript-debounce-function
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-}
-
 class RequestManager {
 	static get(url) {
 		return makeRequest(url);
@@ -29,5 +13,4 @@ class RequestManager {
 
 export default {
 	get: RequestManager.get,
-	debounce: RequestManager.debounce,
 }
