160b139f3610b2b3137d5aed931627bdae09ac7d
Ryan Callaghan <me@ryancallaghan.com.au>
2016-09-06 10:01:02 +1000

    putting props into const and then using that to display data.

--- src/components/TitleInfo.js
+++ src/components/TitleInfo.js
@@ -7,27 +7,28 @@ class Titleinfo extends Component {
 	}
 
 	render() {
+		const data = this.props.data;
 		return (
 			<div>
 				<Homebutton LinkTo='/'/>
 
 				<div className="row">
 					<div className="col-md-3">
-						<img src={this.props.data.Poster} className="poster"/>
+						<img src={data.Poster} className="poster"/>
 					</div>
 					<div className="col-md-9">
-						<h1>{this.props.data.Title}</h1>
-						<p className="rating"><b>Rating on IMDB:</b> {this.props.data.imdbRating}</p>
-						<p className="votes"><b>IMDB Voters:</b> {this.props.data.imdbVotes}</p>
+						<h1>{data.Title}</h1>
+						<p className="rating"><b>Rating on IMDB:</b> {data.imdbRating}</p>
+						<p className="votes"><b>IMDB Voters:</b> {data.imdbVotes}</p>
 						
 						<h3>Plot</h3>
-						<p className="plot">{this.props.data.Plot}</p>
+						<p className="plot">{data.Plot}</p>
 						
 						<h3>Actors</h3>
-						<p className="actors">{this.props.data.Actors}</p>
+						<p className="actors">{data.Actors}</p>
 						
 						<h3>Writers</h3>
-						<p className="writers">{this.props.data.Writer}</p>
+						<p className="writers">{data.Writer}</p>
 					</div>
 				</div>
 			</div>
