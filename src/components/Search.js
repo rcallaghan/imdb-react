03150b8894bacb9a7fedd9e25be006b7b78a89d5
Ryan Callaghan <me@ryancallaghan.com.au>
2016-09-06 11:41:18 +1000

    Removing placeholder/value from state as it isn't needed.

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,21 +7,18 @@ class Search extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			placeholder: 'Search for a title', 
-			value: '',
 			results: [],
 		}
 	}
 
 	handleChange(e) {
-		this.setState({value: e.target.value});
-		RequestManager.get(API_SEARCH_URL + this.state.value).then(payload => this.setState({'results': payload}));
+		RequestManager.get(API_SEARCH_URL + e.target.value).then(payload => this.setState({'results': payload}));
 	}
 
 	render() {
 		return (
 			<div>
-				<input type='text' placeholder={this.state.placeholder} onChange={this.handleChange.bind(this)}/>
+				<input type="text" placeholder="Search for a title" onChange={this.handleChange.bind(this)}/>
 				<List data={this.state.results.Search} />
 			</div>
 		)
