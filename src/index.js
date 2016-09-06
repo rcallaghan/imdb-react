3c32f68c332b525bd945200adcc18227e425831c
Ryan Callaghan <me@ryancallaghan.com.au>
2016-09-06 11:17:51 +1000

    Removing App.js as it is not needed, updated component to use Search.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Router, Route, Link, browserHistory } from 'react-router'
 import { render } from 'react-dom'
-import App from './components/App'
+import Search from './components/Search'
 import Title from './components/Title'
 
 render((
 	<Router history={browserHistory}>
-		<Route path="/" component={App} />
+		<Route path="/" component={Search} />
 	    <Route path="title/:title" component={Title} />
 	</Router>
 ), document.querySelector('.app'))
